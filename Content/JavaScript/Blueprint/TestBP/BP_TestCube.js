3733797cf3eeb46c03a0652577ccf858f2b28278
Ray-player <leiyy414@foxmail.com>
2026-01-21 15:01:03 +0800

    update ts

--- Content/JavaScript/Blueprint/TestBP/BP_TestCube.js
+++ Content/JavaScript/Blueprint/TestBP/BP_TestCube.js
@@ -1,20 +1,26 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.BP_TestCube = void 0;
 const UE = require("ue");
+const mixin_1 = require("../../mixin");
 const puerts_1 = require("puerts");
-//ts覆盖蓝图方法调用示例
-const uclass = UE.Class.Load("/Game/Blueprint/BP_TestCube.BP_TestCube_C");
-const jsClass = puerts_1.blueprint.tojs(uclass);
-class TS_Cube {
-    /* ReceiveBeginPlay(): void {
-        console.log("Message From TS_Cube | Puerts")
-    } */
+// 资产路径
+const AssetPath = "/Game/Blueprint/TestBP/BP_TestCube.BP_TestCube_C";
+// 创建一个继承ts的本体类    implements   实现类型提示
+let BP_TestCube = class BP_TestCube {
+    static StaticClass;
     ReceiveTick(DeltaSeconds) {
         this.K2_AddActorLocalRotation(new UE.Rotator(0, 10 * DeltaSeconds, 0), false, (0, puerts_1.$ref)(), false);
     }
-    TS_FuncPuerts() {
-        console.log("Message From TS_Cube | TS_FuncPrintMessage");
-    }
-}
-puerts_1.blueprint.mixin(jsClass, TS_Cube);
+};
+exports.BP_TestCube = BP_TestCube;
+exports.BP_TestCube = BP_TestCube = __decorate([
+    (0, mixin_1.default)(AssetPath)
+], BP_TestCube);
 //# sourceMappingURL=BP_TestCube.js.map
--- TypeScript/Blueprint/TestBP/BP_TestCube.ts
+++ TypeScript/Blueprint/TestBP/BP_TestCube.ts
@@ -1,22 +1,22 @@
-import * as UE from "ue"
-import {$ref, argv, blueprint } from "puerts"
+﻿import * as UE from "ue";
+import mixin from "../../mixin";
+import { $ref } from "puerts";
 
-//ts覆盖蓝图方法调用示例
-const uclass = UE.Class.Load("/Game/Blueprint/BP_TestCube.BP_TestCube_C")
-const jsClass = blueprint.tojs<typeof UE.Game.Blueprint.TestBP.BP_TestCube.BP_TestCube_C>(uclass)
+// 资产路径
+const AssetPath = "/Game/Blueprint/TestBP/BP_TestCube.BP_TestCube_C";
 
-interface TS_Cube extends UE.Game.Blueprint.TestBP.BP_TestCube.BP_TestCube_C{}
-class TS_Cube implements TS_Cube{
-    /* ReceiveBeginPlay(): void {
-        console.log("Message From TS_Cube | Puerts")
-    } */
+
+// 创建一个继承ts类（或者其他类）的接口（用来类型提示）
+export interface BP_TestCube extends UE.Game.Blueprint.TestBP.BP_TestCube.BP_TestCube_C {
+}
+
+
+// 创建一个继承ts的本体类    implements   实现类型提示
+@mixin(AssetPath)
+export class BP_TestCube implements BP_TestCube {
+    static StaticClass: () => UE.Class;
+
     ReceiveTick(DeltaSeconds: number): void {
         this.K2_AddActorLocalRotation(new UE.Rotator(0, 10 * DeltaSeconds, 0),false,$ref<UE.HitResult>(),false)
     }
-    TS_FuncPuerts(): void {
-        console.log("Message From TS_Cube | TS_FuncPrintMessage")
-    }
 }
-
-blueprint.mixin(jsClass, TS_Cube)
-
